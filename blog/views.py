284495e7c3c2f737fc5a9d5c8a43d44371cc2214
dvbdbb <dovy1@hotmail.co.uk>
2024-06-21 13:15:07 +0100

    Renamed view attributes

--- blog/views.py
+++ blog/views.py
@@ -1,48 +1,70 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.views import View
+from django.views.generic import ListView, DetailView, View
 from .models import Post
 from .forms import SearchForm
 
-class BlogPageView(View):
-    def get(self, request):
+class BlogPageView(ListView):
+    model = Post
+    template_name = "blog_page.html"
+    context_object_name = "paginated_posts"
+    paginate_by = 5
+    paginator_class = "this"
+    ordering = ["-pub_date"]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = SearchForm()
+        context["tree_posts"] = Post.objects.all().order_by('-pub_date')
+        return context
+
+    """def get(self, request):
         posts = Post.objects.all().order_by('-pub_date')
         form = SearchForm()
         paginator = Paginator(posts, 5)
         page = request.GET.get('page')
         try:
-            page_posts = paginator.page(page)
+            paginated_posts = paginator.page(page)
         except PageNotAnInteger:
-            page_posts = paginator.page(1)
+            paginated_posts = paginator.page(1)
         except EmptyPage:
-            page_posts = paginator.page(paginator.num_pages)
-        return render(request, 'blog_page.html', {'posts': posts, 'form': form, 'page_posts': page_posts})
+            paginated_posts = paginator.page(paginator.num_pages)
+        return render(request, 'blog_page.html', {'posts': posts, 'form': form, 'paginated_posts': paginated_posts})"""
 
-class PostDetailView(View):
-    def get(self, request, post_id):
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+    context_object_name = "post"
+    pk_url_kwarg = "post_id"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = SearchForm()
+        context["tree_posts"] = Post.objects.all().order_by("-pub_date")
+        return context
+
+    """def get(self, request, post_id):
         posts = Post.objects.all().order_by('-pub_date')
         post = get_object_or_404(Post, pk=post_id)
         form = SearchForm()
-        return render(request, 'post_detail.html', {'posts': posts, 'post': post, 'form': form})
+        return render(request, 'post_detail.html', {'posts': posts, 'post': post, 'form': form})"""
 
 class SearchView(View):
     def get(self, request):
-        if request.method == "GET":
-            form = SearchForm(request.GET)
-            posts = Post.objects.all().order_by('-pub_date')
-            if form.is_valid():
-                query = form.cleaned_data.get("query")
-                results = Post.objects.filter(title__icontains=query) | Post.objects.filter(content__icontains=query)
-                paginator = Paginator(results, 5)
-                page = request.GET.get('page')
-                try:
-                    page_results = paginator.page(page)
-                except PageNotAnInteger:
-                    page_results = paginator.page(1)
-                except EmptyPage:
-                    page_results = paginator.page(paginator.num_pages)
-                return render(request, 'search_results.html',
-                            {'posts': posts, 'query': query, 'form': form, 'results': page_results})
-            else:
-                form = SearchForm()
-            return render(request, 'search_results.html', {'posts': posts, 'form': form})
+        form = SearchForm(request.GET)
+        tree_posts = Post.objects.all().order_by("-pub_date")
+        if form.is_valid():
+            query = form.cleaned_data.get("query")
+            results = (Post.objects.filter(title__icontains=query) 
+                    | Post.objects.filter(content__icontains=query)).order_by("-pub_date")
+            paginator = Paginator(results, 5)
+            page = request.GET.get("page")
+            try:
+                page_results = paginator.page(page)
+            except PageNotAnInteger:
+                page_results = paginator.page(1)
+            except EmptyPage:
+                page_results = paginator.page(paginator.num_pages)
+            return render(request, "search_results.html",
+                        {"tree_posts": tree_posts, "form": form, "query": query, "results": page_results})
+        return render(request, "search_results.html", {"tree_posts": tree_posts, "form": form})
