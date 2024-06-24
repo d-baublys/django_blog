212901a1f6096d902deb5b21243ee22ba0fad0c0
dvbdbb <dovy1@hotmail.co.uk>
2024-06-24 10:43:27 +0100

    Fix template layout issues and correct template tags.
    - Pagination and footer elements now stay fixed at the bottom of the page
    - Changed <h3> tags in the posts rendering loop to <p>

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y9he9ejre%1xi)2h=pfyd2m#7hhsk-09uhgj3^9i@8o^n(by3x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
