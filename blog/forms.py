dc10f0466590520230c2d7f416f1976bba30b5b1
dvbdbb <dovy1@hotmail.co.uk>
2024-06-25 16:02:40 +0100

    fix: adjust search form width.
    - Changed desktop behaviour so that content doesn't resize when browser is resized.
    - Search form width varies depending on desktop or mobile viewing.
    - Adjusted some margins for more symmetry.

--- blog/forms.py
+++ blog/forms.py
@@ -2,4 +2,9 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    query = forms.CharField(label="", max_length=100)
+    query = forms.CharField(
+        label="",
+        max_length=100,
+        widget=forms.TextInput(attrs={"class": "search-width"})
+    )
+    
