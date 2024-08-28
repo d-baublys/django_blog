f99d6392625f14489115e792923db36f48ee897a
dvbdbb <dovy1@hotmail.co.uk>
2024-08-28 11:28:07 +0100

    refactor: change js scripts from jquery to vanilla

--- blog/static/main.js
+++ blog/static/main.js
@@ -1,22 +1,48 @@
 
-$(document).ready(function () {
-    $('.year, .month').click(function (event) {
-        event.stopPropagation();
-        $(this).toggleClass('reveal');
-        $(this).children('ul').toggle();
+// $(document).ready(function () {
+//     $('.year, .month').click(function (event) {
+//         event.stopPropagation();
+//         $(this).toggleClass('reveal');
+//         $(this).children('ul').toggle();
+//     });
+
+//     $('.date').click(function (event) {
+//         event.stopPropagation();
+//     });
+// });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const yearMonthElements = document.querySelectorAll(".year, .month");
+
+    yearMonthElements.forEach(function (element) {
+        element.addEventListener("click", function (event) {
+            event.stopPropagation();
+            element.classList.toggle("reveal");
+
+            const datesUl = element.querySelector(".dates");
+            if (datesUl) {
+                datesUl.style.display = datesUl.style.display === "none" ? "block" : "none";
+            }
+        })
     });
 
-    $('.date').click(function (event) {
-        event.stopPropagation();
-    });
+    const dateElements = document.querySelectorAll(".date");
+
+    dateElements.forEach(function (element) {
+        element.addEventListener("click", function (event) {
+            event.stopPropagation();
+        })
+    })
 });
 
 const menuButton = document.querySelector(".menu-button");
 const headerMenu = document.querySelector(".header-menu");
-const banner = document.querySelector(".banner");
-const bannerScroll = banner.clientHeight - (menuButton.clientHeight * 1.33);
 
 window.addEventListener("scroll", () => {
+    const banner = document.querySelector(".banner");
+    const bannerScroll = banner.clientHeight - (menuButton.clientHeight * 1.33);
+
     if (window.scrollY > bannerScroll) {
         menuButton.classList.add("sticky");
     } else {
@@ -29,7 +55,7 @@ menuButton.addEventListener("click", () => {
     headerMenu.classList.toggle("active");
 });
 
-document.addEventListener("click", function(event) {
+document.addEventListener("click", function (event) {
     if (!menuButton.contains(event.target) && !headerMenu.contains(event.target)) {
         menuButton.classList.remove("active");
         headerMenu.classList.remove("active");
