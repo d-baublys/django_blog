e8375cde2307ae6b87532e23c021dac768f8dc07
dvbdbb <dovy1@hotmail.co.uk>
2024-09-03 14:34:51 +0100

    feat: only fully reveal most recent year in the post tree

--- blog/static/main.js
+++ blog/static/main.js
@@ -1,20 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const yearMonthElements = document.querySelectorAll(".year, .month");
-    const yearElements = document.querySelectorAll(".year");
-    const currentYear = new Date().getFullYear();
-    const currentMonth = new Date().getMonth();
+document.addEventListener("DOMContentLoaded", () => {
+    const yearMonthLis = document.querySelectorAll(".year, .month");
+    const yearLis = document.querySelectorAll(".year");
+    let mostRecentYear = 0;
 
-    yearElements.forEach(function(yearElement) {
-        const year = parseInt(yearElement.textContent.trim(), 10);
+    yearLis.forEach((yearLi) => {
+        const year = parseInt(yearLi.textContent.trim(), 10);
 
-        if (year === currentYear) {
-            yearElement.classList.add("reveal");
-            const monthsUl = yearElement.querySelector("ul.months");
+        if (year > mostRecentYear) {
+            mostRecentYear = year;
+        }
+    });
+
+    yearLis.forEach((yearLi) => {
+        const year = parseInt(yearLi.textContent.trim(), 10);
+
+        if (year === mostRecentYear) {
+            yearLi.classList.add("reveal");
+            const monthsUl = yearLi.querySelector("ul.months");
             
             monthsUl.style.display = "block";
             const monthLis = monthsUl.querySelectorAll('li.month');
             
-            monthLis.forEach(function(monthLi) {
+            monthLis.forEach((monthLi) => {
                 monthLi.classList.add("reveal");
                 const datesUl = monthLi.querySelector("ul.dates");
                 datesUl.style.display = "block";
@@ -22,42 +29,42 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    yearMonthElements.forEach(function (element) {
-        element.addEventListener("click", function (event) {
+    yearMonthLis.forEach((yearMonthLi) => {
+        yearMonthLi.addEventListener("click", (event) => {
             event.stopPropagation();
-            element.classList.toggle("reveal");
-            const childUl = element.querySelector("ul");
+            yearMonthLi.classList.toggle("reveal");
+            const childUl = yearMonthLi.querySelector("ul");
             
             if (childUl) {
                 childUl.style.display = childUl.style.display === "none" ? "block" : "none";
             }
         })
     });
-});
 
-const menuButton = document.querySelector(".menu-button");
-const headerMenu = document.querySelector(".header-menu");
-const banner = document.querySelector(".banner");
-const bannerScroll = banner.clientHeight - (menuButton.clientHeight * 1.33);
+    const menuButton = document.querySelector(".menu-button");
+    const headerMenu = document.querySelector(".header-menu");
+    const banner = document.querySelector(".banner");
+    const bannerScroll = banner.clientHeight - (menuButton.clientHeight * 1.33);
 
-window.addEventListener("scroll", () => {
-    if (window.scrollY > bannerScroll) {
-        menuButton.classList.add("sticky");
-    } else if (!menuButton.classList.contains("active")) {
-        menuButton.classList.remove("sticky");
-    }
-});
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > bannerScroll) {
+            menuButton.classList.add("sticky");
+        } else if (!menuButton.classList.contains("active")) {
+            menuButton.classList.remove("sticky");
+        }
+    });
 
-menuButton.addEventListener("click", () => {
-    menuButton.classList.toggle("active");
-    headerMenu.classList.toggle("active");
-    menuButton.classList.toggle("sticky");
-});
+    menuButton.addEventListener("click", () => {
+        menuButton.classList.toggle("active");
+        headerMenu.classList.toggle("active");
+        menuButton.classList.toggle("sticky");
+    });
 
-document.addEventListener("click", function (event) {
-    if (!menuButton.contains(event.target) && !headerMenu.contains(event.target)) {
-        menuButton.classList.remove("active");
-        headerMenu.classList.remove("active");
-        menuButton.classList.remove("sticky");
-    }
+    document.addEventListener("click", (event) => {
+        if (!menuButton.contains(event.target) && !headerMenu.contains(event.target)) {
+            menuButton.classList.remove("active");
+            headerMenu.classList.remove("active");
+            menuButton.classList.remove("sticky");
+        }
+    });
 });
