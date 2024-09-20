67a8fc1d1ab73c939c039167f240b9c4586920e3
dvbdbb <dovy1@hotmail.co.uk>
2024-09-20 16:29:50 +0100

    refactor: standardise double quotes for multiple files

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import BlogPageView, PostDetailView, SearchView
 
 urlpatterns = [
-    path('', BlogPageView.as_view(), name='home'),
-    path('search', SearchView.as_view(), name='search'),
-    path('post/<int:post_id>', PostDetailView.as_view(), name='post_detail_view'),
+    path("", BlogPageView.as_view(), name="home"),
+    path("search", SearchView.as_view(), name="search"),
+    path("post/<int:post_id>", PostDetailView.as_view(), name="post_detail_view"),
 ]
