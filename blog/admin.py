827ad67d7e5d3c842b5ce44bd729badcf9aaa045
dvbdbb <dovy1@hotmail.co.uk>
2024-10-29 22:21:46 +0000

    feat: enable full functionality for ckeditor, incl spellchecker, minor change to post p margins

--- blog/admin.py
+++ blog/admin.py
@@ -2,17 +2,14 @@ from django.contrib import admin
 from .models import Post
 
 
-class CustomAdminSite(admin.AdminSite):
-    site_header = "DB's Blog Administration"
-    site_title = "DB's Blog site admin"
-
-
 class PostAdmin(admin.ModelAdmin):
     list_display = ["title", "pub_date", "user"]
     search_fields = ["title"]
     exclude = ["slug"]
 
 
-custom_admin_site = CustomAdminSite()
+admin.site.register(Post, PostAdmin)
 
-custom_admin_site.register(Post, PostAdmin)
+admin.site.site_header = "DB's Blog Administration"
+admin.site.site_title = "Site Admin"
+admin.site.index_title = "Admin Home"
--- blog_site/settings.py
+++ blog_site/settings.py
@@ -133,5 +133,7 @@ CKEDITOR_UPLOAD_PATH = "uploads/"
 CKEDITOR_CONFIGS = {
     'default': {
         'versionCheck': False,
+        'toolbar': 'full',
+        'scayt_autoStartup': True,
     }
 }
