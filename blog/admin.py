afecc7319c79593d378c0e2ec8e5e5bb41f4d5fc
dvbdbb <dovy1@hotmail.co.uk>
2024-10-29 10:42:36 +0000

    feat: implement custom admin site for custom header and titles

--- blog/admin.py
+++ blog/admin.py
@@ -2,10 +2,17 @@ from django.contrib import admin
 from .models import Post
 
 
+class CustomAdminSite(admin.AdminSite):
+    site_header = "DB's Blog Administration"
+    site_title = "DB's Blog site admin"
+
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ["title", "pub_date", "user"]
     search_fields = ["title"]
     exclude = ["slug"]
 
 
-admin.site.register(Post, PostAdmin)
+custom_admin_site = CustomAdminSite()
+
+custom_admin_site.register(Post, PostAdmin)
